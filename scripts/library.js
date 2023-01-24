f53c7c9737ca44d04d73d83590d64c9a64dbbc83
ditod1 <orlandodiazconde@gmail.com>
2023-01-24 13:34:58 -0500

    Refactor to class object creation

--- scripts/library.js
+++ scripts/library.js
@@ -1,60 +1,83 @@
-let bookLibrary = [];
 const bookForm = document.querySelector('.book-form');
 const bookContainer = document.querySelector('.book-container');
 const bookRowTemplate = document.getElementById('book-row-template');
 
-const Book = (title, author) => ({
-  title, author
-});
-
-function libraryFilter(title, author) {
-  return bookLibrary.filter(book => book.title.toLowerCase() === title.toLowerCase()
-    && book.author.toLowerCase() === author.toLowerCase());
+class Book {
+  constructor(title = 'unknown', author = 'unknown') {
+    this.title = title;
+    this.author = author;
+  }
 }
 
-function addBook(e) {
-  const dataTitle = bookForm.querySelector('#title').value;
-  const dataAuthor = bookForm.querySelector('#author').value;
-  currentFilter = libraryFilter(dataTitle, dataAuthor);
-  if (currentFilter.length === 0) {
-    currentBook = Book(dataTitle, dataAuthor);
-    bookLibrary.push(currentBook);
-    addBookFn(currentBook);
-    localStorage.setItem('bookLibrary', JSON.stringify(bookLibrary));
+class Library {
+  constructor() {
+    this.books = [];
+  };
+
+  addBook(newBook) {
+    this.books.push(newBook);
+  };
+
+  removeBook(currentTitle, currentAuthor) {
+    this.books = this.books.filter(book => (book.title.toLowerCase() !== currentTitle.toLowerCase())
+      && (book.author.toLowerCase() !== currentAuthor.toLowerCase()));
+  };
+
+  isBookInLibrary(currentBook) {
+    return this.books.some((book) => book.title === currentBook.title
+      && book.author === currentBook.author);
   }
+}
+
+const library = new Library();
+
+function addBook(e) {
   e.preventDefault();
+  const title = bookForm.querySelector('#title').value;
+  const author = bookForm.querySelector('#author').value;
+  const newBook = new Book(title, author);
+  if (!library.isBookInLibrary(newBook) && !(title.length === 0) && !(author.length === 0)) {
+    library.addBook(newBook);
+    storageSaveLocal();
+    displayAddBook(newBook);
+  }
 }
 
 function removeBook(e) {
-  const currentBook = e.target.parentNode;
   if (e.target.className === 'remove-row') {
+    const currentBook = e.target.parentNode;
     const bookTitle = currentBook.querySelector('.book-title').innerText;
     const bookAuthor = currentBook.querySelector('.book-author').innerText;
-    currentFilter = libraryFilter(bookTitle, bookAuthor);
-    bookLibrary.splice(bookLibrary.indexOf(currentFilter[0]), 1);
+    library.removeBook(bookTitle, bookAuthor);
     e.target.parentNode.remove();
-    localStorage.setItem('bookLibrary', JSON.stringify(bookLibrary));
+    storageSaveLocal();
   }
 }
 
-function addBookFn(book) {
+function storageSaveLocal() {
+  localStorage.setItem('bookLibrary', JSON.stringify(library.books));
+}
+
+function storageLoadSession() {
+  library.books = JSON.parse(localStorage.getItem('bookLibrary'));
+}
+
+function displayAddBook(book) {
   const bookClone = bookRowTemplate.content.cloneNode(true);
   bookClone.querySelector('.book-title').innerText = book.title;
   bookClone.querySelector('.book-author').innerText = book.author;
   bookContainer.appendChild(bookClone);
 }
 
-function displayBooks(books) {
+function DisplayLoadBooks(books) {
   books.forEach((book) => {
-    addBookFn(book);
+    displayAddBook(book);
   });
 }
-
 if (localStorage.getItem('bookLibrary')) {
-  bookLibrary = JSON.parse(localStorage.getItem('bookLibrary'));
+  storageLoadSession();
 }
 
-displayBooks(bookLibrary);
-
+DisplayLoadBooks(library.books);
 bookContainer.addEventListener('click', removeBook);
 bookForm.querySelector('.btn-add-book').addEventListener('click', addBook);
